75028b2ad2d58e3230e6242062e1e83a40c36f64
George Chang <59218296+gchang12@users.noreply.github.com>
2020-11-11 17:01:08 -1000

    Add files via upload
    Added more file-reading functions and fixed ordering algorithm

--- gender_dict.py
+++ gender_dict.py
@@ -58,9 +58,119 @@ def promo_dict(game,is_promo=False):
     return d
 
 
+def hard_mode_dict():
+    elibe_hm='.','metadata',r'elibe_hm.csv'
+    elibe_hm=sep.join(elibe_hm)
+    level_dict={}
+    with open(elibe_hm) as r_file:
+        for line in r_file.readlines():
+            line=line.strip()
+            line=line.split(',')
+            unit=line[0]
+            level=line[1]
+            if not level.isdigit():
+                continue
+            level=int(level)
+            chapter=line[2]
+            if unit not in level_dict.keys():
+                level_dict[unit]={}
+            k=level_dict[unit]
+            k[chapter]=level
+    return level_dict
+
+
+def auto_level_dict():
+    auto_level='.','metadata',r'auto_level.csv'
+    auto_level=sep.join(auto_level)
+    level_dict={}
+    with open(auto_level) as r_file:
+        for line in r_file.readlines():
+            line=line.strip()
+            line=line.split(',')
+            unit=line[0]
+            level=line[3]
+            if not level.isdigit():
+                continue
+            level=int(level)
+            early_chapter=line[1]
+            late_chapter=line[2]
+            if unit not in level_dict.keys():
+                level_dict[unit]={}
+            k=level_dict[unit]
+            k[early_chapter]=0
+            k[late_chapter]=level
+    return level_dict
+
+
+def auto_promo_dict():
+    auto_promo='.','metadata',r'auto_promo.csv'
+    auto_promo=sep.join(auto_promo)
+    promo_dict={}
+    with open(auto_promo) as r_file:
+        for line in r_file.readlines():
+            line=line.strip()
+            line=line.split(',')
+            game=line[0]
+            unit=line[1]
+            if game in promo_dict.keys():
+                promo_dict[game]+=(unit,)
+            else:
+                promo_dict[game]=(unit,)
+    return promo_dict
+            
+
+
+def booster_dict(game):
+    booster_info='.','metadata',r'boosters.csv'
+    booster_info=sep.join(booster_info)
+    start_column=1
+    bonus_dict={}
+    if game == '5':
+        start_column+=2
+    if game == '9':
+        start_column+=4
+    with open(booster_info) as r_file:
+        for line in r_file.readlines():
+            line=line.strip()
+            line=line.split(',')
+            stat=line[0]
+            item=line[start_column]
+            bonus=line[start_column+1]
+            if not bonus.isdigit():
+                continue
+            bonus=int(bonus)
+            bonus_dict[item]=(stat,bonus)
+    return bonus_dict
+
+
+def names_dict(game):
+    name_info='.','metadata',r'updated_names.csv'
+    name_info=sep.join(name_info)
+    names={}
+    with open(name_info) as r_file:
+        for line in r_file.readlines():
+            line=line.strip()
+            line=line.split(',')
+            game_num,old_name,new_name=tuple(line)
+            if game_num != game:
+                if names:
+                    break
+                else:
+                    continue
+            names[old_name]=new_name
+    return names
+
+
+def updated_name_for(game,unit):
+    names=names_dict(game)
+    if unit in names.keys():
+        unit=names[unit]
+    return unit
+
+
 if __name__=='__main__':
-    k=8
+    k=5
     game=str(k)
-    x=gender_dict(game)
-    for item in x.items():
-        print(item)
+    d=names_dict(game)
+    for l in d.items():
+        print(l)
