4642881cade795a53c9e5a2953d24e96aaaf530f
George Chang <59218296+gchang12@users.noreply.github.com>
2020-11-30 09:52:26 -1000

    Add files via upload

--- dracogate2.py
+++ dracogate2.py
@@ -27,20 +27,16 @@ class Aenir:
         self.root=None
 
         self.nwFrame=None
-        self.nwLabel=None
 
         self.neFrame=None
-        self.neLabel=None
         self.infoLabel=None
 
         self.swFrame1=None
-        self.swLabel=None
         self.gsListbox=None
 
         self.swFrame2=None
 
         self.seFrame=None
-        self.seLabel=None
         self.usListbox=None
 
         self.seFrame1=None
@@ -52,55 +48,16 @@ class Aenir:
         self.dHeight2=0
         self.rWidth=0
 
-    def set_mainframe_label(self,text,row,column):
-        label=Label(self.root,text=text)
-        label.grid(sticky=N+W,row=row,column=column)
-        return label
-
-    def set_all_labels(self,nw,ne,sw,se):
-        self.nwLabel=self.set_mainframe_label(nw,0,0)
-        self.neLabel=self.set_mainframe_label(ne,0,1)
-        self.swLabel=self.set_mainframe_label(sw,2,0)
-        self.seLabel=self.set_mainframe_label(se,2,1)
-
-    def set_mainframe(self,row,column,width,height,rowspan=1):
-        kw={
-            'padx':4,\
-            'pady':4,\
-            'borderwidth':2,\
-            'width':width,\
-            'height':height,\
-            'relief':'sunken'
-            }
-        frame=Frame(self.root,**kw)
-        frame.grid_propagate(0)
-        grid_kw={
-            'padx':0,\
-            'pady':0
-            }
-        frame.grid(row=row,column=column,rowspan=rowspan,**grid_kw)
-        return frame
-
     def load_menu(self):
         #   Set root window and frames here
         self.root=Tk()
         self.root.title('Aenir')
         self.root.wm_resizable(width=False,height=False)
-
-        #   ***Failed attempt at adjusting window position
-        #   -   window dimensions inadvertently set to 1x1
-        geometry=self.root.wm_geometry()
-        #   str:    widthxheight+x+y
-        new_geometry=geometry.split('+')
-        #   list:   [widthxheight,x,y]
-        x_pos=new_geometry[1]
-        y_pos=geometry[2]
-        x_pos=2*self.root.winfo_screenwidth()/3
-        y_pos=self.root.winfo_screenheight()/3
-        new_geometry[1]=str(int(x_pos))
-        new_geometry[2]=str(int(y_pos))
-        geometry='+'.join(new_geometry)
-        #self.root.wm_geometry(geometry)
+        
+        x=int(2*self.root.winfo_screenwidth()/3)
+        y=int(self.root.winfo_screenheight()/8)
+        new_geometry='450x620+%d+%d'%(x,y)
+        geometry=self.root.wm_geometry(new_geometry)
 
         #   Create menus here
 
@@ -140,36 +97,17 @@ class Aenir:
         self.quit()
         self.__call__()
 
-    def select_from_list(self,master,itemlist,height,add_scrollbar=False):
-        list_var=StringVar(value=itemlist)
-
-        kwargs={
-            'master':master,\
-            'listvariable':list_var,\
-            'height':height,\
-            'width':30
-            }
-        item_list=Listbox(**kwargs)
-        item_list.grid(column=0,sticky=N+W+S,row=0)
-
-        if add_scrollbar:
-            scrollbar=Scrollbar(master)
-            scrollbar.grid(column=1,sticky=N+S,rowspan=2,row=0)
-            scrollbar.config(command=item_list.yview)
-            item_list['yscrollcommand']=scrollbar.set
-
-        return item_list
-
     def game_select(self):
         master=self.swFrame1
         itemlist=tuple(game_title_dict().keys())
         height=len(itemlist)+1
         args=master,itemlist,height
-        self.gsListbox=self.select_from_list(*args)
+        self.gsListbox=select_from_list(*args)
         self.gsListbox.bind('<<ListboxSelect>>',self.unit_preview)
         self.gsListbox.bind('<Return>',self.confirm_game)
         self.gsListbox.focus_force()
         self.gsListbox.select_set(0)
+        bind_listbox_shortcuts(self.gsListbox)
 
     def unit_preview(self,*args):
         title=self.gsListbox.selection_get()
@@ -203,55 +141,26 @@ class Aenir:
         itemlist=unit_list(game)
         height=28
         args=master,itemlist,height,True
-        self.usListbox=self.select_from_list(*args)
+        self.usListbox=select_from_list(*args)
         self.usListbox['state']=DISABLED
         self.usListbox.bind('<Return>',self.confirm_unit)
-        keys='<End>','<Home>','<Prior>','<Next>'
-        for key in keys:
-            self.usListbox.bind(key,self.jump_in_us)
-
-    def jump_in_us(self,*args):
-        x=args[0].keysym
-        current_index=self.usListbox.curselection()[0]
-        height=self.usListbox['height']
-        length=self.usListbox.size()-1
-        self.usListbox.selection_clear(0,length)
-        if x in ('End','Home'):
-            if x == 'End':
-                target=length
-            else:
-                target=0
-            number=target-current_index
-            self.usListbox.yview_scroll(number,'units')
-        elif x in ('Prior','Next'):
-            number=2
-            if x == 'Prior':
-                target=current_index - height
-                if target < 0:
-                    target=0
-                number=-number
-            else:
-                target=current_index+height
-                if target > length:
-                    target=length
-            self.usListbox.yview_scroll(number,'units')
-        else:
-            return
-        self.usListbox.select_set(target)
+        bind_listbox_shortcuts(self.usListbox)
         
     def option_select(self):
         #   ***Create widgets for each in swFrame:
-        #   Checkbutton:            LYN MODE
-        #   Listbox:                FE4 KID
-        #   Entry:                  HUGH
-        #   Checkbutton/Listbox:    HARD MODE
-        #   Listbox:                AUTO LEVEL
+        #   Checkbutton:                LYN MODE
+        #   Listbox:                    FE4 KID
+        #   Radiobutton:                HUGH
+        #   Checkbutton/Radiobutton:    HARD MODE
+        #   Radiobutton:                AUTO LEVEL
 
         #   ***Create stat-preview pane in seFrame2:
-        #   -   bound above methods
-        #   -   Create Label in seFrame1 pointing to seFrame2
+        #   -   bound to above methods
+        #   -   Create Label in neFrame explaining preview
         info_text='Some information is missing.\nPlease specify:\n\n'
         d=self.unit_params
+        self.seFrame2['text']='Preview'
+        self.swFrame2['text']='Default'
         if is_lyndis_league(**d):
             more_text='Lyn Mode'
         elif is_fe4_child(**d):
@@ -265,8 +174,8 @@ class Aenir:
             swText='Hard Mode'
             seText='Chapter'
             more_text='\n'.join((swText,seText))
-            self.seLabel['text']=seText
-            self.swLabel['text']=swText
+            self.seFrame1['text']=seText
+            self.swFrame1['text']=swText
         elif has_hm_bonus(**d):
             k=hard_mode_dict()[d['unit']]
             if '' in k.keys():
@@ -279,18 +188,14 @@ class Aenir:
             self.quit()
         info_text+=more_text
         if self.unit_params['unit'] != 'Gonzales':
-            self.swLabel['text']=more_text
+            self.swFrame1['text']=more_text
         return info_text
-        
 
-    def anakin(self,frame):
-        assert type(frame) == Frame
-        temple=frame.winfo_children()
-        for youngling in temple:
-            youngling.destroy()
+    def launch_main_menu(self,*args):
+        print('Ctrl+F: def launch_main_menu')
+        print('args: ',args)
 
     def confirm_unit(self,*args):
-        #   ***Create OK button in Game Select
         self.usListbox.unbind('<Return>')
         self.usListbox['state']=DISABLED
 
@@ -302,16 +207,39 @@ class Aenir:
 
         self.update_config()
 
-        self.seLabel['text']=''
+        self.seFrame['text']=''
 
-        self.anakin(self.swFrame1)
+        anakin(self.swFrame1)
         self.seFrame.destroy()
-        self.seFrame1=self.set_mainframe(3,1,self.rWidth,self.dHeight1)
-        self.seFrame2=self.set_mainframe(4,1,self.rWidth,self.dHeight2)
+        self.seFrame1=set_mainframe(3,1,self.rWidth,self.dHeight1)
+        self.seFrame2=set_mainframe(4,1,self.rWidth,self.dHeight2)
 
         if not self.game_unit_check():
-            info_text='Please confirm your selection.\n\nF5: Restart session\nEsc: Quit session'
-            self.swLabel['text']='Confirm'
+            info_text='F5: Restart session\n\nEsc: Quit session'
+            self.swFrame1['text']='Confirm'
+            self.swFrame2['text']='Current Stats'
+            
+            button_cfg={
+                'master':self.swFrame1,\
+                'text':'OK',\
+                'width':23,\
+                'height':1,\
+                'command':self.launch_main_menu #   Launch main menu
+                }
+
+            notification='Please confirm your selection.\n\nUse any of the shortcut keys in\nthe upper-right pane at any time\nduring the session.'
+            Label(self.swFrame1,text=notification,justify=LEFT).grid(row=0,sticky=E+W)
+
+            button_gcfg={
+                'sticky':NSEW,\
+                'ipadx':10,\
+                'ipady':10,\
+                'row':1
+                }
+            ok_button=Button(**button_cfg)
+            ok_button.grid(**button_gcfg)
+            ok_button.focus()
+            ok_button.bind('<Return>',self.launch_main_menu)
         else:
             info_text=self.option_select()
 
@@ -325,7 +253,7 @@ class Aenir:
             kw_list=[{'color':None} for stat in y.my_stats]
         else:
             frame=self.seFrame2
-            self.anakin(self.seFrame2)
+            anakin(self.seFrame2)
             y=kishuna
             comparison=y > self.my_unit
             kw_list=[]
@@ -357,8 +285,6 @@ class Aenir:
             show_stat_pair(**kw)
 
     def update_config(self,frame=None,color=None):
-        #   ***Must center-justify stats somehow...
-
         display_pairs=list(self.display_params.items())
         label,value=display_pairs[-1]
         row=len(display_pairs)-1
@@ -367,17 +293,24 @@ class Aenir:
         grid_options1['row']=row
         grid_options2=grid_options1.copy()
 
-        label_options={}
-
         if frame is None:
             frame=self.nwFrame
             grid_options1['sticky']=N+W
             grid_options2['sticky']=N+E
+
         if color is not None:
             grid_options2.update({'foreground':color})
 
-        Label(frame,text=label,**label_options).grid(column=0,**grid_options1)
-        Label(frame,text=value,**label_options).grid(column=1,**grid_options2)
+        label_options={}
+        label_options['master']=frame
+        #label_options['justify']=RIGHT
+        #label_options['anchor']='e'
+
+        grid_options1['column']=0
+        grid_options2['column']=1
+
+        Label(text=label,**label_options).grid(**grid_options1)
+        Label(text=value,**label_options).grid(**grid_options2)
 
     def game_unit_check(self):
         d=self.unit_params
@@ -392,9 +325,8 @@ class Aenir:
 
     def __call__(self):
         self.load_menu()
-        self.set_all_labels('Config','Info','Game Select','Unit Select')
 
-        dy=100
+        dy=120
 
         lWidth=200
         rWidth=210
@@ -412,18 +344,18 @@ class Aenir:
 
         assert dHeight1+dHeight2 == dHeight
 
-        self.nwFrame=self.set_mainframe(1,0,lWidth,uHeight)
+        self.nwFrame=set_mainframe(1,0,lWidth,uHeight,text='Config')
 
-        self.neFrame=self.set_mainframe(1,1,rWidth,uHeight)
+        self.neFrame=set_mainframe(1,1,rWidth,uHeight,text='Info')
         self.infoLabel=Label(self.neFrame,text='Please select a game.',justify=LEFT)
         self.infoLabel.grid(sticky=N+W)
 
-        self.swFrame1=self.set_mainframe(3,0,lWidth,dHeight1)
+        self.swFrame1=set_mainframe(3,0,lWidth,dHeight1,text='Game Select')
         self.game_select()
 
-        self.swFrame2=self.set_mainframe(4,0,lWidth,dHeight2)
+        self.swFrame2=set_mainframe(4,0,lWidth,dHeight2)
 
-        self.seFrame=self.set_mainframe(3,1,rWidth,dHeight,2)
+        self.seFrame=set_mainframe(3,1,rWidth,dHeight,2,text='Unit Select')
         self.unit_select()
 
         self.root.mainloop()
