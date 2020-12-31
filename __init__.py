6a727d355389e24c123b9d3947433bb9a63ef490
George Chang <59218296+gchang12@users.noreply.github.com>
2020-12-31 00:46:46 -1000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,20 +3,22 @@
 from aenir2.dracogate import Aenir
 from aenir2.quintessence import Morph
 
-from importlib import machinery
-
 #   Must change cwd to module directory
+
+from importlib.machinery import PathFinder
 
 from os import chdir
 from os.path import sep
 
-aenir_loc=machinery.PathFinder()
+aenir_loc=PathFinder()
 aenir_loc=aenir_loc.find_spec('aenir2')
 aenir_loc=aenir_loc.origin
 aenir_loc=aenir_loc.split(sep)
 aenir_loc=sep.join(aenir_loc[:-1])
 
 chdir(aenir_loc)
+
+#   Print notification of directory change
 
 message=(
     'Current working directory has been changed to:',\
