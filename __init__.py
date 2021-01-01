f2d6cee587e77ebc2f2c6608044b0fb1b16db881
George Chang <59218296+gchang12@users.noreply.github.com>
2021-01-01 12:18:48 -1000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,33 +3,6 @@
 from aenir2.dracogate import Aenir
 from aenir2.quintessence import Morph
 
-#   Must change cwd to module directory
-
-from importlib.machinery import PathFinder
-
-from os import chdir, getcwd
-from os.path import sep
-
-aenir_loc=PathFinder()
-aenir_loc=aenir_loc.find_spec('aenir2')
-aenir_loc=aenir_loc.origin
-aenir_loc=aenir_loc.split(sep)
-aenir_loc=sep.join(aenir_loc[:-1])
-
-owd=getcwd()
-chdir(aenir_loc)
-
-#   Print notification of directory change
-
-message=(
-    'Current working directory has been changed to:',\
-    aenir_loc,\
-    'Data files now accessible by module.',\
-    'Original working directory stored in parameter \'owd\'.',\
-    ''
-    )
-message='\n\n'.join(message)
-print(message)
-
 if __name__ == '__main__':
     repo='https://test.pypi.org/project/aenir2/'
+    print(owd)
