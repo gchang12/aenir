501f7f65224b90c611c1518dec7468599f9984b0
George Chang <59218296+gchang12@users.noreply.github.com>
2021-04-13 21:45:21 -1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
-#   ***Provide docstrings for both classes
-
 from aenir2.dracogate import Aenir
 from aenir2.quintessence import Morph
 
 if __name__ == '__main__':
     repo='https://test.pypi.org/project/aenir2/'
-    print(owd)
+    print(repo)
