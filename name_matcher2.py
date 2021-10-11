952645d24d3a3fa0acb72c2590fb2e9d32ed891a
George Chang <59218296+gchang12@users.noreply.github.com>
2021-10-10 21:30:23 -1000

    Add files via upload

--- name_matcher2.py
+++ name_matcher2.py
@@ -5,7 +5,7 @@ from os.path import sep
 from aenir2.gender_dict import gender_dict
 
 class NameMatcher:
-    def __init__(self,game,unit):
+    def __init__(self,game,unit,mounted=None):
         folder=sep.join(['.','class_data'])
         filePointer=lambda x: sep.join([folder,'fe%s'%game,x])
         dict_file=filePointer('class_dict.json')
@@ -15,6 +15,7 @@ class NameMatcher:
         self.gender=gender_dict(game)[unit]
         self.name_dict=self.fetchDict(dict_file)
         self.rank_dict=self.fetchDict(rank_file)
+        self.mounted=mounted
 
     def fetchDict(self,filename):
         with open(filename,'r') as rfile:
@@ -25,7 +26,12 @@ class NameMatcher:
         if self.game == '7':
             if 'Wagon' in self.gender:
                 return
-        unit_classG=unit_class+self.gender
+        gender=self.gender
+        unit_classG=unit_class+gender
+        if type(mounted) == bool and game == '5':
+            if mounted:
+                unit_classG=unit_classG[:-1]+', D)'
+                unit_class=unit_class+' (D)'
         ranks=self.rank_dict.keys()
         if self.game in ('7','8'):
             lord_list='Eliwood','Hector','Lyn','Ephraim','Eirika'
@@ -39,7 +45,7 @@ class NameMatcher:
             if unit_class not in self.name_dict.keys():
                 return
             new_class=self.name_dict[unit_class]
-            new_classG=new_class+self.gender
+            new_classG=new_class+gender
             if new_class in ranks:
                 return new_class
             elif newclassG in ranks:
