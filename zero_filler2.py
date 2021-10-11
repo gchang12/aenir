bcdbe907147980c65da4e1f61c87c5408fe53399
George Chang <59218296+gchang12@users.noreply.github.com>
2021-10-11 10:30:04 -1000

    Add files via upload

--- zero_filler2.py
+++ zero_filler2.py
@@ -8,17 +8,22 @@ from aenir2.data_dict import read_stat_names
 
 class ZeroFiller:
     def __init__(self,game,folder=None):
+        assert type(folder) in (type(None),tuple,str)
         self.game=game
         self.stat_dict=read_stat_names(game)
         if folder is None:
             self.folder='.'
+        elif type(folder) == tuple:
+            self.folder=sep.join(folder)
         elif folder == 'metadata':
             self.folder=folder
         else:
-            assert type(folder) == str
             self.folder=sep.join(['.',folder,'fe%s'%game])
 
     def fillDict(self,stats,values):
+        if (type(stats),type(values)) == (str,int):
+            stats=(stats,)
+            values=(values,)
         assert len(stats) == len(values)
         my_dict=dict()
         for old_name,new_name in self.stat_dict.items():
@@ -40,7 +45,7 @@ class ZeroFiller:
         values=array(values)
         return values
 
-    def fromCSV(self,row_name,filename,valtype='Series'):
+    def fromCSV(self,row_name,filename,return_as='Series'):
         filename=sep.join([self.folder,filename])
         data=pd.read_csv(filename,index_col=0)
         assert row_name in data.index
@@ -51,24 +56,24 @@ class ZeroFiller:
         values=tuple(d.values())
         stats=tuple(d.keys())
         data=pd.Series(data=values,index=stats,name=row_name)
-        if valtype == 'array':
+        if return_as == 'array':
             x=data.to_numpy()
-        elif valtype == 'Series':
+        elif return_as == 'Series':
             x=data
-        elif valtype == 'dict':
+        elif return_as == 'dict':
             x=data.to_dict()
         else:
-            assert valtype in ('array','Series','dict')
+            assert return_as in ('array','Series','dict')
         return x
 
-    def fromDict(self,d,valtype='dict'):
+    def fromDict(self,d,return_as='dict'):
         assert type(d) == dict
-        if valtype == 'dict':
+        if return_as == 'dict':
             func=self.fillDict
-        elif valtype == 'array':
+        elif return_as == 'array':
             func=self.fillArray
         else:
-            assert valtype in ('dict','array')
+            assert return_as in ('dict','array')
         values=tuple(d.values())
         stats=tuple(d.keys())
         return func(stats,values)
@@ -80,6 +85,6 @@ if __name__ == '__main__':
     filename='characters_growth-rates.csv'
     x=ZeroFiller(game,folders)
     #d={'HP':99}
-    #y=x.fromDict(d,valtype='array')
+    #y=x.fromDict(d,return_as='array')
     y=x.fromCSV(unit,filename)
     print(y.to_dict())
