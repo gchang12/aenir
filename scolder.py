c66879b965b5c33dae628ef8ca8ede3b6ed6cfc8
George Chang <59218296+gchang12@users.noreply.github.com>
2021-10-14 00:20:36 -1000

    Add files via upload
    Corrected typo in scolder.py and recycled some variables in Morph class

--- scolder.py
+++ scolder.py
@@ -2,7 +2,7 @@ def scold_user(message,iterable=None):
     if iterable is not None:
         for item in iterable:
             print(item)
-    if type(messsage) == str:
+    if type(message) == str:
         message=('',message,'')
     else:
         message=('',*message,'')
