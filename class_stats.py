f9180996b19f607ef60c7572b4ad2a9b82235c43
George Chang <59218296+gchang12@users.noreply.github.com>
2021-10-15 11:45:24 -1000

    Add files via upload

--- class_stats.py
+++ class_stats.py
@@ -1,11 +1,13 @@
 import json
+import pandas as pd
 
 from os.path import sep
 
 from aenir2.class_matcher import ClassMatcher
 from aenir2.growths_parser import GrowthsParser
+from aenir2.rank_fetcher2 import RankFetcher2
 
-class ClassGrowths:
+class ClassStats:
     def __init__(self,game):
         self.game=game
         self.main_dir=sep.join(['.','class_data','fe%s'%game])
@@ -20,21 +22,42 @@ class ClassGrowths:
     def inputFile(self,filename):
         return sep.join([self.main_dir,filename])
 
-    def getGrowths(self,unit_class):
+    def getGrowths(self,unit_class,uncommon=None):
+        assert unit_class in self.name_list
         if self.game in ('4','5'):
             x=GrowthsParser(self.game)
             if self.game == '4':
                 func=x.getGrowths4
             else:
-                func=x.getGrowths5
+                func=lambda y: x.getGrowths5(y,uncommon=uncommon)
         else:
             x=ClassMatcher(self.game)
             func=x.getGrowths
         return func(unit_class)
 
+    def getBases(self,unit_class):
+        file=self.inputFile('classes_base-stats.csv')
+        data=pd.read_csv(file,index_col=0)
+        row=data.loc[unit_class,:]
+        return row.to_numpy()
+
+    def getRanks(self,unit_class):
+        rf2=RankFetcher2(self.game)
+        ranks=rf2.ranksFromFile()[unit_class]
+        return tuple(ranks)
+
+    def getPromo(self,unit_class):
+        # Match bases to promo-names first
+        x=4
+
+    def getMaxes(self,unit_class):
+        # Match promo to maxes-names first
+        x=4
+
 if __name__ == '__main__':
     game='4'
     unit_class='Swordfighter'
     x=ClassGrowths(game)
-    y=x.getGrowths(unit_class)
+    #y=x.getGrowths(unit_class)
+    y=x.getBases(unit_class)
     print(y)
---
+++ release-notes.md
@@ -0,0 +1,31 @@
+Tentative release. Combat simulation scripts are WIP and maybe post-poned.
+
+SCRAPING
+====
+- Scrape boss data for each game
+- Scrape support data
+- Scrape character and class skills for all games
+
+DATA-MATCHING
+===
+- Match class-bases to class-promo
+- Match class-promo to class-maxes
+- Create GenericMorph object for random mooks
+
+COMBAT SIMULATION
+===
+- Special weapons (magic; reaver; effective; etc.)
+- Classes weak to certain weapons
+- Damage algorithm implementation including WTA and so forth
+- Algorithmically determine inherited skills in FE4 and what-not
+- Add terrain data to make this task way more overwhelming
+- Add name-updating module just for extra deterrent
+- Leadership stars affecting avoid and hit-rate
+- Skills and option to use items that teach skills
+
+AFTERTHOUGHTS
+===
+- Test out scripts written thus far
+- Put off next steps (writing new Morph class) until all's been settled
+- Clean up all code written recently
+- UnitDirectory class; look-up units/classes by classes/usable weapons/promotion status; etc.
