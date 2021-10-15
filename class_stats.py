0d82ccdda8d4c052e270b6ac5fba635dce86b76b
George Chang <59218296+gchang12@users.noreply.github.com>
2021-10-15 11:51:00 -1000

    Add files via upload

--- class_stats.py
+++ class_stats.py
@@ -8,7 +8,7 @@ from aenir2.growths_parser import GrowthsParser
 from aenir2.rank_fetcher2 import RankFetcher2
 
 class ClassStats:
-    def __init__(self,game):
+    def __init__(self,game,unit_class):
         self.game=game
         self.main_dir=sep.join(['.','class_data','fe%s'%game])
         if game == '4':
@@ -22,7 +22,7 @@ class ClassStats:
     def inputFile(self,filename):
         return sep.join([self.main_dir,filename])
 
-    def getGrowths(self,unit_class,uncommon=None):
+    def getGrowths(self,uncommon=None):
         assert unit_class in self.name_list
         if self.game in ('4','5'):
             x=GrowthsParser(self.game)
@@ -35,29 +35,37 @@ class ClassStats:
             func=x.getGrowths
         return func(unit_class)
 
-    def getBases(self,unit_class):
+    def getBases(self):
         file=self.inputFile('classes_base-stats.csv')
         data=pd.read_csv(file,index_col=0)
         row=data.loc[unit_class,:]
         return row.to_numpy()
 
-    def getRanks(self,unit_class):
+    def getRanks(self):
         rf2=RankFetcher2(self.game)
         ranks=rf2.ranksFromFile()[unit_class]
         return tuple(ranks)
 
-    def getPromo(self,unit_class):
+    def getPromo(self):
         # Match bases to promo-names first
         x=4
 
-    def getMaxes(self,unit_class):
-        # Match promo to maxes-names first
+    def getMaxes(self):
+        # Match bases and promo to maxes-names first
         x=4
+
+    def getSkills(self):
+        # Get skills from FE5,8,9 first
+        # - FE4 has character skills which are inherited and stuff
+        x=5
+
+    def getWeaknesses(self):
+        # Manually write text file containing weaknesses
+        x=44
 
 if __name__ == '__main__':
     game='4'
     unit_class='Swordfighter'
     x=ClassGrowths(game)
-    #y=x.getGrowths(unit_class)
     y=x.getBases(unit_class)
     print(y)
