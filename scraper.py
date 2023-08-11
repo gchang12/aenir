bee4df81019354a8d2c41ea3b4e36a0f4c037468
gchang12 <gc8@hawaii.edu>
2023-08-11 06:03:25 -0700

    extra tests and checks

--- scraper.py
+++ scraper.py
@@ -5,11 +5,13 @@ Stores data tables into SQL databases.
 """
 
 import logging
-import typing
 import urllib.parse
 
 import requests as r
 import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 class SerenesScraper:
@@ -19,27 +21,45 @@ class SerenesScraper:
     """
     # URL_ROOT := {scheme://netloc}
     URL_ROOT = 'https://serenesforest.net/'
-    # Assert that the arguments are of specific types
-    def __init__( self, path ):
+    def __init__(self, game_name: str):
         """
         Initialize:
         - home_url: Determines whence to begin scraping operations.
         - table_dict: section/page -> list[pd.DataFrame]
         """
-        # Parameters and such -> DEBUG
-        self.home_url = urllib.parse.urljoin( self.URL_ROOT, path )
+        if not isinstance(game_name, str):
+            raise TypeError
+        logging.info("__init__(self, '%s');", game_name)
+        self.game_name = game_name
+        logging.info("self.game_name = '%s';", self.game_name)
+        self.home_url = urllib.parse.urljoin(self.URL_ROOT, game_name)
+        logging.info("self.home_url = '%s';", self.home_url)
+        logging.info("response = requests.get(url=self.home_url);")
+        response = r.get(self.home_url)
+        logging.info("response.raise_for_status();")
+        response.raise_for_status()
+        logging.info("No exception was raised;")
         self.table_dict = {}
+        logging.info("self.table_dict = {};")
 
-    def scrape_tables( self, section, page ):
+    def scrape_tables(self, section: str, page: str):
         """
         Scrapes table objects from home_url/section/page.
         """
-        # Operations -> INFO
-        table_url = urllib.parse.urljoin( self.home_url, section )
-        table_url = urllib.parse.urljoin( table_url, page )
-        response = r.get( table_url )
+        for arg in (section, page):
+            if not isinstance(arg, str):
+                raise TypeError
+        logging.info("self.scrape_tables(section='%s', page='%s');", section, page)
+        table_url = urllib.parse.urljoin(self.home_url, section)
+        table_url = urllib.parse.urljoin(table_url, page)
+        logging.info("response = requests.get(url='%s');", table_url)
+        response = r.get(table_url)
+        logging.info("response.raise_for_status();")
         response.raise_for_status()
-        self.table_dict[ '/'.join( [ section, page ] ) ] = pd.read_html( response.text )
+        logging.info("No exception was raised;")
+        url_key = '_'.join([section, page])
+        self.table_dict[url_key] = pd.read_html(response.text)
+        logging.info("table_dict['%s'] = pd.read_html(response.text);", url_key)
 
 if __name__ == '__main__':
     breakpoint()
