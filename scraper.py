ab904b66703fd12ecdefec418801683383ef8b30
gchang12 <gc8@hawaii.edu>
2023-08-16 23:13:19 -0700

    All functions functioning as expected. Now to compile data.

--- scraper.py
+++ scraper.py
@@ -3,15 +3,14 @@
 Fetches data tables off https://serenesforest.net/
 Stores data tables into SQL databases.
 """
+# pylint: disable=W3101
 
 import logging
-import urllib.parse
 
 import requests as r
 import pandas as pd
 
-from _base import SerenesBase
-
+from aenir._base import SerenesBase
 
 class SerenesScraper(SerenesBase):
     """
@@ -26,11 +25,15 @@ class SerenesScraper(SerenesBase):
         - url_to_tables: section/page -> list[pd.DataFrame]
         """
         if not isinstance(game_name, str):
+            logging.info("game_name='%s' is not a str.", game_name)
             raise TypeError
         SerenesBase.__init__(self, game_name)
-        self.home_url = urllib.parse.urljoin(self.URL_ROOT, game_name)
+        self.home_url = "/".join((self.URL_ROOT, game_name))
+        logging.info("self.home_url = \"%s\"", self.home_url)
+        logging.info("requests.get(\"%s\")", self.home_url)
         response = r.get(self.home_url)
         response.raise_for_status()
+        logging.info("requests.raise_for_status(): No anomalies")
 
     def scrape_tables(self, path: str):
         """
@@ -40,8 +43,11 @@ class SerenesScraper(SerenesBase):
             raise TypeError
         table_url = "/".join([self.home_url, path])
         response = r.get(table_url)
+        logging.info("requests.get(\"%s\")", table_url)
         response.raise_for_status()
+        logging.info("requests.raise_for_status(): No anomalies")
         self.url_to_tables[path] = pd.read_html(response.text)
+        logging.info("self.url_to_tables[\"%s\"] := list[pd.DataFrame]", path)
 
     def save_tables(self, urlpath: str):
         """
@@ -50,28 +56,34 @@ class SerenesScraper(SerenesBase):
         tablename = self.urlpath_to_tablename(urlpath)
         tableindex = 0
         self.home_dir.mkdir(exist_ok=True, parents=True)
-        save_dir = self.home_dir.joinpath("raw_stats.db").__str__()
+        logging.info("'%s' directory now exists.", str(self.home_dir))
+        save_dir = str(self.home_dir.joinpath("raw_stats.db"))
         while self.url_to_tables[urlpath]:
             table = self.url_to_tables[urlpath].pop(0)
             name = tablename + str(tableindex)
             con = "sqlite:///" + save_dir
             table.to_sql(name, con, index=False)
+            logging.info("table.to_sql(\"%s\", \"%s\", index=False)", name, con)
             tableindex += 1
         del self.url_to_tables[urlpath]
+        logging.info("\"%s\" no longer in self.url_to_tables.", urlpath)
 
     def load_tables(self, tablename: str):
         """
         Loads table data and junk
         """
-        save_dir = self.home_dir.joinpath("raw_stats.db").__str__()
-        self.url_to_tables[self.tablename_to_urlpath(tablename)] = []
+        save_dir = str(self.home_dir.joinpath("raw_stats.db"))
+        urlpath = self.tablename_to_urlpath(tablename)
+        self.url_to_tables[urlpath] = []
         tableindex = 0
         while True:
             table_name = tablename + str(tableindex)
             con = "sqlite:///" + save_dir
+            logging.info("table = pd.read_sql_table%s", (table_name, con))
             try:
                 table = pd.read_sql_table(table_name, con)
-                self.url_to_tables[self.tablename_to_urlpath(tablename)].append(table)
+                logging.info("Appending 'table' to self.url_tables[\"%s\"]", urlpath)
+                self.url_to_tables[urlpath].append(table)
                 tableindex += 1
             except ValueError:
                 break
