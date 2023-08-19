cfdb5eeba75cbdb1b3bffae72eebc3064bb179e2
gchang12 <gc8@hawaii.edu>
2023-08-18 19:37:18 -0700

    tests refactored to accommodate for the refactoring of _base and scraper.load_tables

--- test_scraper.py
+++ test_scraper.py
@@ -26,26 +26,14 @@ class TestScraper(unittest.TestCase):
         self.sos_scraper = scraper.SerenesScraper(6)
         self.sos_scraper.home_dir.joinpath("raw_stats.db").unlink(missing_ok=True)
 
-    def test__init__gamename_isnotstr(self):
+    def test__init__gamenum_notin_numtoname(self):
         """
         Tests that no scraper.SerenesScraper instance is created
         if the game_name is not a string.
         """
-        logging.info("Asserting that __init__ fails when its argument is not a string.")
-        with self.assertRaises(TypeError):
+        logging.info("Asserting that __init__ fails when its argument is not in NUM_TO_NAME.")
+        with self.assertRaises(KeyError):
             nonexistent_scraper = scraper.SerenesScraper(None)
-        with self.assertRaises(NameError):
-            del nonexistent_scraper
-
-    def test__init__gamename_notexists(self):
-        """
-        Tests that no scraper.SerenesScraper instance is created
-        if the game name does not exist as a path on SF.net.
-        """
-        logging.info("Asserting that __init__ fails when its str-argument is not a game.")
-        bad_name = 'yabba-dabba-doo'
-        with self.assertRaises(r.exceptions.HTTPError):
-            nonexistent_scraper = scraper.SerenesScraper(bad_name)
         with self.assertRaises(NameError):
             del nonexistent_scraper
 
@@ -75,7 +63,7 @@ class TestScraper(unittest.TestCase):
             del no_name
         self.assertDictEqual(self.sos_scraper.url_to_tables, {})
 
-    def test_scrape_tables__path_exists(self):
+    def test_scrape_tables(self):
         """
         Asserts that the 'url_to_tables' object is appended
         with a list of pd.DataFrame objects.
@@ -101,7 +89,6 @@ class TestScraper(unittest.TestCase):
         section_page = "characters/growth-rates"
         self.sos_scraper.scrape_tables(section_page)
         self.sos_scraper.save_tables(section_page)
-        # key should be cleared after the save.
         self.assertNotIn(section_page, self.sos_scraper.url_to_tables)
         db_path = self.sos_scraper.home_dir.joinpath("raw_stats.db")
         self.assertTrue(db_path.exists())
@@ -129,14 +116,12 @@ class TestScraper(unittest.TestCase):
         from one fetched from the worldwide web.
         """
         logging.info("Asserting that load_tables succeeds.")
-        table_name = "characters__growth_rates"
-        urlpath = self.sos_scraper.tablename_to_urlpath(table_name)
+        urlpath = "characters/growth-rates"
         self.sos_scraper.scrape_tables(urlpath)
         original_table = self.sos_scraper.url_to_tables[urlpath][0]
         self.sos_scraper.save_tables(urlpath)
-        self.sos_scraper.load_tables(table_name)
+        self.sos_scraper.load_tables(urlpath)
         self.assertIn(urlpath, self.sos_scraper.url_to_tables)
-        url_source = "/".join( [self.sos_scraper.home_url, urlpath] )
         loaded_table = self.sos_scraper.url_to_tables[urlpath][0]
         self.assertTrue(all(loaded_table == original_table))
 
