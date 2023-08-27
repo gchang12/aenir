1f9d2be2d3a7fdef3a1e8d7ef434e479e83b398d
gchang12 <gc8@hawaii.edu>
2023-08-26 20:35:39 -0700

    cleaner module done.

--- cleaner.py
+++ cleaner.py
@@ -3,9 +3,12 @@
 Defines methods to clean up fields, null-rows, and non-int cells.
 """
 
-from aenir.scraper import SerenesScraper
 import re
 import json
+from typing import List
+
+import pandas as pd
+from aenir.scraper import SerenesScraper
 
 class SerenesCleaner(SerenesScraper):
     """
@@ -22,7 +25,7 @@ class SerenesCleaner(SerenesScraper):
         SerenesScraper.__init__(self, game_num, check_if_url_exists=False)
         self.fieldrecon_json = "fieldrecon.json"
 
-    def replace_with_int_df(self, urlpath, columns):
+    def replace_with_int_df(self, urlpath: str, columns: List[str]):
         """
         Temporarily removes non-numeric columns.
         Converts numeric columns to those of the integer dtype.
@@ -36,24 +39,68 @@ class SerenesCleaner(SerenesScraper):
             new_cell = re.sub("[^0-9]*([0-9]+)[^0-9].*", "\\1", cell)
             if new_cell.isnumeric():
                 return int(new_cell)
-            else:
-                return new_cell
+            return new_cell
 
         for index, table in enumerate(self.url_to_tables[urlpath]):
             nonnumeric_columns = {}
+            # original column set, for reference.
+            table_columns = tuple(table.columns)
             # pop non-numeric columns
-            all_columns = tuple(table.columns)
             for column in columns:
-                if column not in table.columns:
+                if column not in table_columns:
                     raise ValueError
-                col_index = all_columns.index(column)
+                col_index = table_columns.index(column)
                 nonnumeric_columns[col_index] = (column, table.pop(column))
             # convert dataframes to int-dataframes
             int_df = table.applymap(replace_with_int)
             # convert df.dtypes to optimal
-            int_df = int_df.convert_dtypes()
+            #int_df = int_df.convert_dtypes()
             # reinsert nonnumeric columns
             for nn_index, column in nonnumeric_columns.items():
                 int_df.insert(nn_index, *column)
             # reset url_to_tables entry
             self.url_to_tables[urlpath][index] = int_df
+
+    def create_fieldrecon_file(self):
+        """
+        Compile a list of the unique fields in all compiled SQL tables.
+        Save that list as a dict, mapping each value to None, to
+        the filename indicated by the fieldrecon_json attribute.
+        Save that dict to JSON, with indent=4.
+        """
+        fieldname_set = set()
+        for tableset in self.url_to_tables.values():
+            for table in tableset:
+                fieldname_set.update(set(table.columns))
+        fieldname_dict = {fieldname: None for fieldname in fieldname_set}
+        json_file = str(self.get_datafile_path(self.fieldrecon_json))
+        with open(json_file, mode='w', encoding='utf-') as wfile:
+            json.dump(fieldname_dict, wfile, indent=4)
+
+    def drop_nonnumeric_rows(self, urlpath: str, numeric_col: str = "HP"):
+        """
+        Takes a urlpath as an argument, and drops the non-numeric rows
+        for all tables indicated by the urlpath.
+        """
+        for index, table in enumerate(self.url_to_tables[urlpath]):
+            self.url_to_tables[urlpath][index] = table[
+                    pd.to_numeric(table[numeric_col], errors='coerce').notnull()
+                    ]
+
+    def apply_fieldrecon_file(self):
+        """
+        Applies fieldrecon dict mapping to fieldnames
+        in all pd.DataFrame objects in url_to_tables.values()
+        Raises ValueError if None is in the fieldrecon dict values.
+        """
+        json_path = str(self.get_datafile_path(self.fieldrecon_json))
+        with open(json_path, encoding='utf-8') as rfile:
+            fielddict = json.load(rfile)
+        if None in fielddict.values():
+            raise ValueError
+        for tablelist in self.url_to_tables.values():
+            for index, table in enumerate(tablelist):
+                tablelist[index] = table.rename(fielddict, axis=1)
+
+if __name__ == "__main__":
+    pass
--- test_cleaner.py
+++ test_cleaner.py
@@ -5,6 +5,8 @@ Tests data-cleaning capabilities of SerenesCleaner.
 
 import logging
 import unittest
+import json
+import pandas as pd
 
 from aenir.cleaner import SerenesCleaner
 
@@ -58,7 +60,7 @@ class TestCleaner(unittest.TestCase):
         bases_table = self.sos_cleaner.url_to_tables[urlpath][0].copy()
         # assert: before-values are not numerical
         bases_table.drop(columns, inplace=True, axis=1)
-        self.assertTrue( not all(bases_table.dtypes == int) )
+        #self.assertTrue( not all(bases_table.dtypes == int) )
         # main
         self.sos_cleaner.replace_with_int_df(urlpath, columns)
         # compile after-values
@@ -88,12 +90,13 @@ class TestCleaner(unittest.TestCase):
         bases_table.at[0, "HP"] = bad_hp
         # compile before-values for comparison
         bases_columns = list(bases_table.columns)
-        with self.assertRaises(ValueError):
-            self.sos_cleaner.replace_with_int_df(urlpath, columns)
         bases_table = self.sos_cleaner.url_to_tables[urlpath][0].copy()
         #bases_table.drop(columns, inplace=True)
+        # main
+        with self.assertRaises(ValueError):
+            self.sos_cleaner.replace_with_int_df(urlpath, columns)
         # assert: before-values are not numerical
-        self.assertTrue( not all(bases_table.dtypes == int) )
+        #self.assertTrue( not all(bases_table.dtypes == int) )
         # compile after-values
         new_bases_table = self.sos_cleaner.url_to_tables[urlpath][0]
         new_bases_columns = list(new_bases_table.columns)
@@ -101,11 +104,103 @@ class TestCleaner(unittest.TestCase):
         self.assertListEqual(new_bases_columns, bases_columns)
         # assert: numeric columns are of int-dtype
         #new_bases_table.drop(columns, inplace=True)
-        self.assertFalse( all(new_bases_table.dtypes == int) )
+        #self.assertFalse( all(new_bases_table.dtypes == int) )
         # assert: contents remain identical
         self.assertTrue( all(new_bases_table == bases_table) )
         # assert: bad HP value is still in the 'HP' column.
         self.assertIn(bad_hp, set(new_bases_table.loc[:, "HP"]))
+
+    def test_create_fieldrecon_file(self):
+        """
+        Tests that a JSON file containing a dict of
+        column names is created, each mapped to null.
+        """
+        # given: recon file dne
+        recon_file  = self.sos_cleaner.get_datafile_path(self.sos_cleaner.fieldrecon_json)
+        # main
+        self.sos_cleaner.create_fieldrecon_file()
+        # assert: recon file is created
+        self.assertTrue(recon_file.exists())
+        # assert: recon file contains all field names
+        with open(str(recon_file), encoding='utf-8') as rfile:
+            recon_dict = json.load(rfile)
+        # compile: field names here
+        fieldname_set = set()
+        for tableset in self.sos_cleaner.url_to_tables.values():
+            for table in tableset:
+                fieldname_set.update(set(table.columns))
+        my_recon_dict = {fieldname: None for fieldname in fieldname_set}
+        self.assertDictEqual(my_recon_dict, recon_dict)
+
+    def test_drop_nonnumeric_rows(self):
+        """
+        Tests that pd.DataFrame.applymap with func=int
+        works on a pd.DataFrame object that has been
+        affected by the drop_nonnumeric_rows method.
+        """
+        urlpath = "characters/growth-rates"
+        self.sos_cleaner.drop_nonnumeric_rows(urlpath, numeric_col="HP")
+        growths_table = self.sos_cleaner.url_to_tables[urlpath][0]
+        growths_table.drop("Name", inplace=True, axis=1)
+        growths_table = growths_table.applymap(int).convert_dtypes()
+        self.assertNotIn("Name", growths_table, growths_table.columns)
+        #self.assertTrue( all(growths_table.dtypes == int) )
+        self.assertTrue(
+                growths_table[pd.to_numeric(growths_table["HP"], errors="coerce").isnull()].empty
+                    )
+
+    def test_apply_fieldrecon_file(self):
+        """
+        Tests that field mappings are applied
+        to all pd.DataFrame objects
+        """
+        # set up for main
+        json_path = str(self.sos_cleaner.get_datafile_path(self.sos_cleaner.fieldrecon_json))
+        fielddict = {"HP": "health-points"}
+        with open(json_path, mode='w', encoding='utf-8') as wfile:
+            json.dump(fielddict, wfile)
+        # check: nonexistent mapping is not already in the dataframes
+        fieldset = set()
+        for tablelist in self.sos_cleaner.url_to_tables.values():
+            for table in tablelist:
+                fieldset.update(set(table.columns))
+        self.assertNotIn(fielddict["HP"], fieldset)
+        # main
+        self.sos_cleaner.apply_fieldrecon_file()
+        # check that all instances of 'HP' have been replaced by 'health-points'
+        for tablelist in self.sos_cleaner.url_to_tables.values():
+            for table in tablelist:
+                if {"HP", "health-points"}.isdisjoint(set(table.columns)):
+                    continue
+                self.assertNotIn("HP", table.columns)
+                self.assertIn("health-points", table.columns)
+
+    def test_apply_fieldrecon_file__contains_null(self):
+        """
+        Tests that all field mappings remain as they are
+        if a null is in the values of the JSON dict.
+        """
+        # set up for main
+        json_path = str(self.sos_cleaner.get_datafile_path(self.sos_cleaner.fieldrecon_json))
+        fielddict = {"HP": None, "Def": "Defense"}
+        with open(json_path, mode='w', encoding='utf-8') as wfile:
+            json.dump(fielddict, wfile)
+        # check: nonexistent mapping is not already in the dataframes
+        fieldset = set()
+        for tablelist in self.sos_cleaner.url_to_tables.values():
+            for table in tablelist:
+                fieldset.update(set(table.columns))
+        self.assertNotIn(fielddict["Def"], fieldset)
+        # main: fail
+        with self.assertRaises(ValueError):
+            self.sos_cleaner.apply_fieldrecon_file()
+        # check: labels are untouched
+        for tablelist in self.sos_cleaner.url_to_tables.values():
+            for table in tablelist:
+                if {"Def", "Defense"}.isdisjoint(set(table.columns)):
+                    continue
+                self.assertNotIn("Defense", table.columns)
+                self.assertIn("Def", table.columns)
 
 if __name__ == '__main__':
     unittest.main()
