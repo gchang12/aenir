61f4799e0d3d6a2de20fac8c91f0f2856742c2e2
gchang12 <gc8@hawaii.edu>
2023-09-06 20:18:43 -0700

    Updated _base module containing the base class.

--- _base.py
+++ _base.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-Defines the SerenesBase class
 """
-# pylint: disable=R0903
 
 from pathlib import Path
 
 class SerenesBase:
     """
-    Defines data directories for use by
-    descendants, including the scraper
-    and data management classes.
     """
 
     _NUM_TO_NAME = {
@@ -31,9 +26,6 @@ class SerenesBase:
 
     def __init__(self, game_num: int):
         """
-        Initialize:
-        - game_name: One of the folders in the serenesforest.net site
-        - home_dir: pathlib.Path object to the directory
         """
         self._game_num = game_num
         self._page_dict = self.URL_TO_TABLE.copy()
@@ -44,63 +36,56 @@ class SerenesBase:
     @property
     def URL_TO_TABLE(self):
         """
-        Get:    {urlpath: urlpath.replace("/", "__").replace("-", "_")
         """
         return self._URL_TO_TABLE
 
     @property
     def NUM_TO_NAME(self):
         """
-        Get:    {num: game-title as it appears on SF.net}
         """
         return self._NUM_TO_NAME
 
     @property
     def game_num(self):
         """
-        Get:    The number of the game in the Fire Emblem series.
         """
         return self._game_num
 
     @property
     def page_dict(self):
         """
-        Get:    dict that maps urlpath -> table_name
         """
         return self._page_dict
 
     @property
     def game_name(self):
         """
-        Get:    str representing the name of the game as shown in the urlpath.
         """
         return self._game_name
 
     @property
     def home_dir(self):
         """
-        Get:    pathlib.Path to the directory where all scraped data for the game is stored.
         """
         return self._home_dir
 
     @property
     def url_to_tables(self):
         """
-        Get:    dict that maps urlpath -> List[stat_tables]
         """
         return self._url_to_tables
 
     def get_datafile_path(self, filename: str):
         """
-        Returns a path to a data file in the home directory
         """
         return self.home_dir.joinpath(filename)
 
     def get_urlname(self, tablename: str):
         """
-        Maps: str -> str.replace(self, "__", "/").replace("_", "-")
         """
-        return tablename.replace("__", "/").replace("_", "-")
+        urlname = tablename.replace("__", "/").replace("_", "-")
+        assert urlname in self.page_dict
+        return urlname
 
 if __name__ == '__main__':
     pass
