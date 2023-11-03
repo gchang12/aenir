ad8b1db7e24e7757d1d54723e368ead4ce0cf6c9
gchang12 <gc8@hawaii.edu>
2023-11-02 22:16:14 -0700

    Realized that the urls.py files in the application folders are NOT relative to the root / {app_name}.

--- aenir3_web/stat_compare/urls.py
+++ aenir3_web/stat_compare/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "stat_compare"
 
 urlpatterns = [
-    path("index/", views.index, name="index"),
-    path("create/", views.create, name="create"),
-    path("edit/", views.edit, name="edit"),
-    path("compare/", views.compare, name="compare"),
+    path(f"{app_name}/", views.index, name="index"),
+    path(f"{app_name}/create/", views.create, name="create"),
+    path(f"{app_name}/edit/", views.edit, name="edit"),
+    path(f"{app_name}/compare/", views.compare, name="compare"),
 ]
