87aab490022f54203ac40c82daa9518d70bd5da6
gchang12 <gc8@hawaii.edu>
2023-11-05 20:03:30 -0800

    Updated templates. Erased work on compare_stats method.

--- aenir3_web/stat_compare/urls.py
+++ aenir3_web/stat_compare/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "stat_compare"
 
 urlpatterns = [
-    path(f"{app_name}/", views.index, name="index"),
-    path(f"{app_name}/create/", views.create, name="create"),
-    path(f"{app_name}/edit/", views.edit_index, name="edit"),
-    path(f"{app_name}/edit/<str:morph_id>", views.edit_morph),
-    path(f"{app_name}/compare/", views.compare, name="compare"),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("edit/", views.edit_index, name="edit"),
+    path("edit/<str:morph_id>", views.edit_morph),
+    path("compare/", views.compare, name="compare"),
 ]
