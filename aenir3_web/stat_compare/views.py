f266db1c748f4efbb1485b49b89707a00cc0c1ae
gchang12 <gc8@hawaii.edu>
2023-11-09 21:20:52 -0800

    Fixed bug with datadir_root in Morph4: Non-kid units automatically fetched data from default directory

--- aenir3_web/stat_compare/views.py
+++ aenir3_web/stat_compare/views.py
@@ -1,30 +1,58 @@
+#!/usr/bin/python3
+"""
+"""
+
 from collections import OrderedDict
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import (
+    FireEmblemGame, # game_num, game_name, display_name
+    FireEmblemUnit # game_num, unit_name, display_name, campaign, father_name
+    )
+from aenir.morph import (
+        BaseMorph,
+        Morph4, Morph5, Morph6, Morph7, Morph8, Morph9
+        )
+
 # Create your views here.
 
 class StatCompare:
+    """
+    """
     QUINTESSENCE = OrderedDict()
+
+    # for use in Morph methods
+    datadir_root = "./stat_compare/static/stat_compare/data/"
 
     @classmethod
     def index(cls, request):
         """
         Contains a compendium of links to the application to access.
         """
-        return render(request, "stat_compare/index.html", context={})
+        import os
+        print(os.getcwd())
+        #BaseMorph(4, cls.datadir_root)
+        sigurd = Morph4("Sigurd", datadir_root=cls.datadir_root)
+        return render(request, "stat_compare/index.html", context={})#{"view_name": sigurd.unit_name })
 
     @classmethod
     def create(cls, request):
+        """
+        """
         # Implement global 'QUINTESSENCE' dict to create Morph object.
         # Show like a different set of controls every time data is sent.
         return render(request, "stat_compare/create.html", context={})
 
     @classmethod
     def edit(cls, request):
+        """
+        """
         return render(request, "stat_compare/edit.html", context={})
 
     @classmethod
     def compare(cls, request):
+        """
+        """
         return render(request, "stat_compare/compare.html", context={})
