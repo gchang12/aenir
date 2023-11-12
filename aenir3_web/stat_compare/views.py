5303a6750add98f798c17e2cfa882d7510f95ca6
gchang12 <gc8@hawaii.edu>
2023-11-11 18:41:27 -0800

    Put in a crude idea of what I want. Still need to conditionally fetch resultset based on game. Still figuring out bugs with POST request.

--- aenir3_web/stat_compare/views.py
+++ aenir3_web/stat_compare/views.py
@@ -6,12 +6,14 @@ from collections import OrderedDict
 
 from django.http import HttpResponse
 from django.shortcuts import render
+# to fix some weirdness with POST data handling
+from django.utils.datastructures import MultiValueDictKeyError
 
 from .models import (
     FireEmblemGame, # game_num, game_name, display_name
     FireEmblemUnit # game_num, unit_name, display_name, campaign, father_name
     )
-from .forms import GameSelect
+from .forms import GameSelect, UnitSelect, get_UNITSELECT_CHOICES
 from aenir.morph import (
         #BaseMorph,
         Morph4, Morph5, Morph6, Morph7, Morph8, Morph9
@@ -26,6 +28,14 @@ class StatCompare:
 
     # for use in Morph methods
     DATADIR_ROOT = "./stat_compare/static/stat_compare/data/"
+    
+    DISPLAY_DICT = {
+            'game_num': "Game",
+            'unit_name': "Unit",
+            }
+
+    # for use in rendering
+    ABOUT_TABLE = []
 
     @classmethod
     def index(cls, request):
@@ -42,12 +52,32 @@ class StatCompare:
         # Implement global 'QUINTESSENCE' dict to create Morph object.
         # Show like a different set of controls every time data is sent.
         context = {"view_name": "Create"}
-        form = GameSelect()
+        forms_to_fill = OrderedDict()
+        forms_to_fill.update({"game_num": GameSelect}
+            )
+        forms_to_fill.update({"unit_name": UnitSelect})
+        for morph_field in forms_to_fill:
+            if morph_field in cls.QUINTESSENCE:
+                continue
+            break
+        form = forms_to_fill[morph_field]()
         context.update({"form": form})
-        print(cls.QUINTESSENCE)
+        context.update({"about_table": cls.ABOUT_TABLE})
         if request.method == "POST":
-            game_num = int(request.POST["display_name"])
-            cls.QUINTESSENCE.update({"game_num": game_num})
+            morph_data = request.POST["display_name"]
+            if morph_data.isnumeric():
+                morph_data = int(morph_data)
+            cls.QUINTESSENCE.update({morph_field: morph_data})
+            if morph_field == "game_num":
+                morph_data = FireEmblemGame.objects.get(game_num=morph_data).display_name
+            cls.ABOUT_TABLE.append(
+                    (cls.DISPLAY_DICT[morph_field], morph_data)
+                    )
+        elif request.method == "GET":
+            cls.QUINTESSENCE.clear()
+        else:
+            # TODO: What do I do if the user makes anything other than a POST or GET request?
+            raise Exception
         return render(request, "stat_compare/create.html", context=context)
 
     @classmethod
