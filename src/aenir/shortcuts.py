62ae8e70a59a7065a1c0d8b45f38362fd3de90c6
gchang12 <gc8@hawaii.edu>
2024-02-27 18:44:03 -0800

    added shortcuts. methods to get options for modes.

--- src/aenir/shortcuts.py
+++ src/aenir/shortcuts.py
@@ -3,11 +3,16 @@
 """
 
 import aenir.morph
+from aenir._basemorph import BaseMorph
 
 def get_morph(game_num: int, unit_name: str, **kwds):
     morph_cls = getattr(aenir.morph, "Morph" + str(game_num))
     morph_inst = morph_cls(unit_name, **kwds)
     return morph_inst
+
+def get_character_list(game_num: int, *, datadir_root: str = "data"):
+    basemorph_inst = BaseMorph(game_num, datadir_root=datadir_root)
+    return basemorph_inst.get_character_list()
 
 
 if __name__ == '__main__':
