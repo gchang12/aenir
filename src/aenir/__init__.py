cfb592435d7e554825fa5c538f6281a862c3da62
gchang12 <gc8@hawaii.edu>
2024-05-30 21:04:37 -0700

    added some essential classes and functions to __init__

--- src/aenir/__init__.py
+++ src/aenir/__init__.py
@@ -1,5 +1,10 @@
 import sys
 
+from aenir.morph import (
+    Morph4, Morph5, Morph6, Morph7, Morph8, Morph9
+)
+from aenir.shortcuts import get_morph
+
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
