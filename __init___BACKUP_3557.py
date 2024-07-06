785ecf469c00b83b0ced28292c977cc6a6b48bc5
gchang12 <gc8@hawaii.edu>
2024-07-05 18:10:54 -0700

    test

---
+++ __init___BACKUP_3557.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Package for comparing the stats of FE[4-9] units.
+
+Simulates level-ups, promotions, among other things.
+"""
+
+if __name__ == '__main__':
+    # https://github.com/gchang12/aenir
+    pass
+    # test
