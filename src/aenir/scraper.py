a3691d488d4694e3e4c028f5e5339a1cf2957adb
gchang12 <gc8@hawaii.edu>
2024-07-05 18:01:58 -0700

    fixed warnings from tox

--- src/aenir/scraper.py
+++ src/aenir/scraper.py
@@ -5,6 +5,7 @@ Defines the SerenesScraper class.
 SerenesScraper: Defines a single method for web-scraping SF.net.
 """
 
+import io
 import logging
 
 import requests
@@ -47,7 +48,8 @@ class SerenesScraper(SerenesBase):
         #!will raise requests.exceptions.HTTPError if name does not exist
         response.raise_for_status()
         logging.info("SerenesScraper.url_to_tables['%s'] = pd.read_html(response.text)", urlpath)
-        self.url_to_tables[urlpath] = pd.read_html(response.text)
+        self.url_to_tables[urlpath] = pd.read_html(io.StringIO(response.text))
+        #self.url_to_tables[urlpath] = pd.read_html(response.text)
         logging.info(
             "%d table(s) found, and loaded into url_to_tables['%s'].",
             len(self.url_to_tables[urlpath]), urlpath
