3d20e87e551ef48f44a7f0363934b19c061bc17e
gchang12 <gc8@hawaii.edu>
2025-05-08 06:33:07 -0700

    added rudimentary logging module

--- src/aenir/logging.py
+++ src/aenir/logging.py
@@ -2,3 +2,36 @@
 """
 
 import logging
+
+logger = logging.getLogger()
+
+# TODO: Configure this!
+
+def configure_logging():
+    """
+    """
+    import logging.config
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "formatter-id": {
+                    "format": None,
+                }
+            },
+            "filters": {
+                "filter-id": {
+                    "filter": None,
+                }
+            },
+            "handlers": {
+                "handler-id": None,
+            },
+            "loggers": {
+                "level": None,
+                "propagate": None,
+                "filters": [],
+                "handlers": [],
+            }
+        }
+    )
