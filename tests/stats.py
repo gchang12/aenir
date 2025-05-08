e194ec87139fd6e80ec433debb3d39bfd5d52ba5
gchang12 <gc8@hawaii.edu>
2025-05-08 07:21:55 -0700

    added tests for iadd and lt

--- tests/stats.py
+++ tests/stats.py
@@ -13,6 +13,254 @@ from aenir.stats import (
 from aenir.logging import logger
 
 # TODO: Pare down on list of fields in  source files.
+# TODO: To be tested = [lt, iadd, min, max]
+
+class StatsTests(unittest.TestCase):
+    """
+    Demo of methods of a functional subclass of AbstractStats.
+    """
+
+    class FunctionalStats(AbstractStats):
+        """
+        Functional in that the 'STAT_LIST' class method is defined.
+        """
+        @classmethod
+        def STAT_LIST(cls):
+            """
+            Returns tuple of strings that can be used as Python identifiers.
+            """
+            return (
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+            )
+
+    class FunctionalStats2(AbstractStats):
+        """
+        Functional in that the 'STAT_LIST' class method is defined. Exists to
+        demonstrate that stats of different classes cannot be operated on even if
+        they share the same 'STAT_LIST'.
+        """
+        @classmethod
+        def STAT_LIST(cls):
+            """
+            Returns tuple of strings that can be used as Python identifiers.
+            """
+            return (
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+            )
+
+    def setUp(self):
+        """
+        Initializes minimal kwargs for initialization of FunctionalStats.
+        """
+        self.stats_class = self.FunctionalStats
+        self.init_kwargs = {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0,
+        }
+        # 1.odd > 2.odd; 1.even > 2.even
+        self.statdict1 = {
+            "a": 18,
+            "b": 7,
+            "c": 5,
+            "d": 9,
+            "e": 2,
+            "f": 5,
+            "g": 1,
+        }
+        self.statdict2 = {
+            "a": 1,
+            "b": 8,
+            "c": 2,
+            "d": 10,
+            "e": 1,
+            "f": 8,
+            "g": 0,
+        }
+
+    def test_lt(self):
+        """
+        Tests that a Stats object of booleans is returned.
+        """
+        expected_stats = self.FunctionalStats(
+            **{
+                "a": False,
+                "b": True,
+                "c": False,
+                "d": True,
+                "e": False,
+                "f": True,
+                "g": False,
+            }
+        )
+        stats1 = self.FunctionalStats(**self.statdict1)
+        stats2 = self.FunctionalStats(**self.statdict2)
+        actual_stats = stats1.lt(stats1, stats2)
+        for attrname in ("a", "b", "c", "d", "e", "f", "g"):
+            actual = getattr(actual_stats, attrname)
+            expected = getattr(expected_stats, attrname)
+            self.assertIs(actual, expected)
+
+    def test_iadd(self):
+        """
+        Tests that the Stats object is incremented.
+        """
+        expected_stats = self.FunctionalStats(
+            **{
+                "a": 19,
+                "b": 15,
+                "c": 7,
+                "d": 19,
+                "e": 3,
+                "f": 13,
+                "g": 1,
+            }
+        )
+        stats1 = self.FunctionalStats(**self.statdict1)
+        stats2 = self.FunctionalStats(**self.statdict2)
+        none = stats1.iadd(stats2)
+        self.assertIsNone(none)
+        actual_stats = stats1
+        for attrname in ("a", "b", "c", "d", "e", "f", "g"):
+            actual = getattr(actual_stats, attrname)
+            expected = getattr(expected_stats, attrname)
+            self.assertEqual(actual, expected)
+
+    def test_lt__different_classes(self):
+        """
+        Asserts that lt method fails if operands are of different Stats
+        classes, and even if the STAT_LIST for both are identical.
+        """
+        # copy-paste to lt
+        stats1 = self.FunctionalStats(**self.statdict1)
+        stats2 = self.FunctionalStats2(**self.statdict2)
+        self.assertTupleEqual(stats1.STAT_LIST(), stats2.STAT_LIST())
+        with self.assertRaises(AssertionError):
+            stats1.lt(stats2, stats1)
+        with self.assertRaises(AssertionError):
+            stats2.lt(stats1, stats2)
+
+    def test_iadd__different_classes(self):
+        """
+        Asserts that iadd method fails if operands are of different Stats
+        classes, and even if the STAT_LIST for both are identical.
+        """
+        # copy-paste to lt
+        stats1 = self.FunctionalStats(**self.statdict1)
+        stats2 = self.FunctionalStats2(**self.statdict2)
+        self.assertTupleEqual(stats1.STAT_LIST(), stats2.STAT_LIST())
+        with self.assertRaises(AssertionError):
+            stats1.iadd(stats2)
+        with self.assertRaises(AssertionError):
+            stats2.iadd(stats1)
+
+    def test_get_stat_dict__eq_stat_list(self):
+        """
+        Tests that key-set in stat-dict is identical to STAT_LIST.
+        """
+        actual = set(self.stats_class.get_stat_dict(0))
+        expected = set(self.stats_class.STAT_LIST())
+        self.assertSetEqual(actual, expected)
+
+    def test_get_stat_dict(self):
+        """
+        Tests that key-set in stat-dict is identical to stats listed in setUp.
+        """
+        actual = set(self.stats_class.get_stat_dict(0))
+        expected = set(self.init_kwargs)
+        self.assertSetEqual(actual, expected)
+
+    def test_init__missing_kwarg(self):
+        """
+        """
+        kwargs = self.init_kwargs
+        hp = kwargs.pop('a')
+        test_class = self.stats_class
+        with self.assertRaises(TypeError) as type_err:
+            test_class(**kwargs)
+
+    def test_init__unexpected_kwarg(self):
+        """
+        """
+        stat_dict = self.init_kwargs
+        stat_dict['unexpected_kwarg'] = None
+        #with self.assertLogs(logger, logging.CRITICAL) as log_ctx:
+        test_class = self.stats_class
+        with self.assertLogs(logger, logging.WARNING) as log_ctx:
+            stats_obj = test_class(**stat_dict)
+
+class ImplementedStatsTests(unittest.TestCase):
+    """
+    Tests that subclasses contain the expected stat-attributes.
+    """
+
+    def test_genealogy_stats(self):
+        """
+        Stats with growth rates in FE4: Genealogy of the Holy War.
+        """
+        expected = (
+            "HP",
+            "Str",
+            "Mag",
+            "Skl",
+            "Spd",
+            "Lck",
+            "Def",
+            "Res",
+        )
+        actual = GenealogyStats.STAT_LIST()
+        self.assertTupleEqual(actual, expected)
+
+    def test_gba_stats(self):
+        """
+        Stats with growth rates in FE6-8.
+        """
+        expected = (
+            "HP",
+            "Pow",
+            "Skl",
+            "Spd",
+            "Lck",
+            "Def",
+            "Con",
+            "Mov",
+        )
+        actual = GBAStats.STAT_LIST()
+        self.assertTupleEqual(actual, expected)
+
+    def test_thracia_stats(self):
+        """
+        Stats with growth rates in FE5: Thracia 776.
+        """
+        expected = (
+            "HP",
+            "Str",
+            "Mag",
+            "Skl",
+            "Spd",
+            "Lck",
+            "Def",
+            "Con",
+            "Mov",
+        )
+        actual = ThraciaStats.STAT_LIST()
+        self.assertTupleEqual(actual, expected)
 
 class AbstractStatsTests(unittest.TestCase):
     """
@@ -80,7 +328,7 @@ class AbstractStatsTests(unittest.TestCase):
         #with self.assertRaises(AttributeError) as assert_err:
         test_obj = test_class(**stat_dict)
         test_obj_period = getattr(test_obj, ".")
-        logger.warning(
+        logger.debug(
             "Hidden attribute has been defined: getattr(%s(**%s)., '.') = %d",
             test_class.__name__, stat_dict, test_obj_period,
         )
@@ -110,134 +358,3 @@ class AbstractStatsTests(unittest.TestCase):
         with self.assertRaises(TypeError) as type_err:
             self.NoStatListStats()
 
-class StatsTests(unittest.TestCase):
-    """
-    Demo of methods of a functional subclass of AbstractStats.
-    """
-
-    class FunctionalStats(AbstractStats):
-        """
-        Functional in that the 'STAT_LIST' class method is defined.
-        """
-        @classmethod
-        def STAT_LIST(cls):
-            """
-            Returns tuple of strings that can be used as Python identifiers.
-            """
-            return (
-                "a",
-                "b",
-                "c",
-                "d",
-                "e",
-                "f",
-                "g",
-            )
-
-    def setUp(self):
-        """
-        Initializes minimal kwargs for initialization of FunctionalStats.
-        """
-        self.stats_class = self.FunctionalStats
-        self.init_kwargs = {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 0,
-            "e": 0,
-            "f": 0,
-            "g": 0,
-        }
-
-    def test_get_stat_dict__eq_stat_list(self):
-        """
-        Tests that key-set in stat-dict is identical to STAT_LIST.
-        """
-        actual = set(self.stats_class.get_stat_dict(0))
-        expected = set(self.stats_class.STAT_LIST())
-        self.assertSetEqual(actual, expected)
-
-    def test_get_stat_dict(self):
-        """
-        Tests that key-set in stat-dict is identical to stats listed in setUp.
-        """
-        actual = set(self.stats_class.get_stat_dict(0))
-        expected = set(self.init_kwargs)
-        self.assertSetEqual(actual, expected)
-
-    def test_init__missing_kwarg(self):
-        """
-        """
-        kwargs = self.init_kwargs
-        hp = kwargs.pop('a')
-        test_class = self.stats_class
-        with self.assertRaises(TypeError) as type_err:
-            test_class(**kwargs)
-
-    def test_init__unexpected_kwarg(self):
-        """
-        """
-        stat_dict = self.init_kwargs
-        stat_dict['unexpected_kwarg'] = None
-        #with self.assertLogs(logger, logging.CRITICAL) as log_ctx:
-        test_class = self.stats_class
-        with self.assertLogs(logger, logging.WARNING) as log_ctx:
-            stats_obj = test_class(**stat_dict)
-
-
-class ImplementedStatsTests(unittest.TestCase):
-    """
-    Tests that subclasses contain the expected stat-attributes.
-    """
-
-    def test_genealogy_stats(self):
-        """
-        Stats with growth rates in FE4: Genealogy of the Holy War.
-        """
-        expected = (
-            "HP",
-            "Str",
-            "Mag",
-            "Skl",
-            "Spd",
-            "Lck",
-            "Def",
-            "Res",
-        )
-        actual = GenealogyStats.STAT_LIST()
-        self.assertTupleEqual(actual, expected)
-
-    def test_gba_stats(self):
-        """
-        Stats with growth rates in FE6-8.
-        """
-        expected = (
-            "HP",
-            "Pow",
-            "Skl",
-            "Spd",
-            "Lck",
-            "Def",
-            "Con",
-            "Mov",
-        )
-        actual = GBAStats.STAT_LIST()
-        self.assertTupleEqual(actual, expected)
-
-    def test_thracia_stats(self):
-        """
-        Stats with growth rates in FE5: Thracia 776.
-        """
-        expected = (
-            "HP",
-            "Str",
-            "Mag",
-            "Skl",
-            "Spd",
-            "Lck",
-            "Def",
-            "Con",
-            "Mov",
-        )
-        actual = ThraciaStats.STAT_LIST()
-        self.assertTupleEqual(actual, expected)
