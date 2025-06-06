98afa8c455e9729aeb67dfb0d5546f70be1c5a51
gchang12 <gc8@hawaii.edu>
2025-06-06 07:42:41 -0700

    added pseudo-logging to csv-to-db file

--- apply_csv_to_db.py
+++ apply_csv_to_db.py
@@ -21,12 +21,14 @@ def grab_csv_data(filename):
     with open(filename) as rfile:
         data = list(csv.DictReader(rfile))
     map(_convert_to_numeric, data)
+    print(data[0])
     return data
 
 def update_db(filename, table, data):
     """
     """
     # identify which fields are numeric; which are not
+    print(table)
     key_fields = list()
     value_fields = list()
     for field, value in data[0].items():
@@ -34,11 +36,14 @@ def update_db(filename, table, data):
             key_fields.append(field)
         elif isinstance(value, int):
             value_fields.append(field)
+    print(key_fields, value_fields)
     # TODO: How to put in query parameters...
     # field=:field as %r, ...
     action = ", ".join(field + "=:" + field for field in value_fields)
     condition = ", ".join(field + "=:" + field for field in key_fields)
     statement = "UPDATE TABLE %s SET %s WHERE %s ;" % (table, action, condition)
+    print(statement)
+    print(data[0])
     with sqlite3.connect(filename) as cnxn:
         cnxn.executemany(statement, data)
 
